f5b5854bc2b3612a79143d7c3d39b7910ac75deb
pl4cydo <psl2@discente.ifpe.edu.br>
2023-05-28 22:14:07 -0300

    finally make the get works

---
+++ v2/database/migrations/2023_05_29_004032_create_books_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('books', function (Blueprint $table) {
+            $table->id();
+            $table->timestamps();
+            $table->string('title');
+            $table->string('year');
+            $table->string('writter');
+            $table->binary('image', 65535)->nullable();
+            $table->foreignId('user_id');
+            $table->string('category_id'); 
+            $table->boolean('deal')->default(false);
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('books');
+    }
+};
---
+++ v2/resources/js/Pages/Category.vue
@@ -0,0 +1,80 @@
+<script setup>
+import InputLabel from '@/Components/InputLabel.vue';
+import PrimaryButton from '@/Components/PrimaryButton.vue';
+import TextInput from '@/Components/TextInput.vue';
+import AuthenticatedLayout from '@/Layouts/AuthenticatedLayout.vue';
+import { Head, useForm } from '@inertiajs/vue3';
+
+
+
+defineProps({
+    categories: Array,
+});
+
+const form = useForm({
+    name: ''
+});
+
+const submit = () => {
+    form.post(route('category.store'))
+    form.name = ''
+}
+
+
+
+// const categories = reactive([]);
+
+// onMounted(async () => {
+//   try {
+//     const response = await axios.get(route('category.index')); // Replace 'axios' with your preferred HTTP client
+//     categories.value = response.data;
+//   } catch (error) {
+//     console.error(error);
+//   }
+// });
+
+
+</script>
+
+<template>
+    <Head title="Category" />
+
+    <AuthenticatedLayout>
+        <template #header>
+            <h2 class="font-semibold text-xl text-gray-800 leading-tight">Category</h2>
+          </template>
+          
+          <div class="py-12">
+            <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
+              <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
+                <div class="p-6 text-gray-900">Add Category</div>
+                <div class="p-6">
+                  <form name="createForm" @submit.prevent="submit" class="flex">
+                    <label for="name" class="mr-5 text-xl">Name</label>
+                    <input
+                      id="name"
+                      v-model="form.name"
+                      class="border border-gray-300 rounded-md px-3 py-2"
+                    />
+                    <button
+                      type="submit"
+                      class="px-4 py-2 ml-4 text-white bg-blue-500 rounded-md hover:bg-blue-600"
+                    >
+                      Send
+                    </button>
+                  </form>
+                </div>
+              </div>
+            </div>
+            <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
+              <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
+                <div class="p-6 text-gray-900">Categories:</div>
+                <div v-for="category in categories" class="p-6">
+                  <h1 class="text-xl">{{ category.name }}</h1>
+                </div>
+              </div>
+            </div>
+          </div>
+              
+    </AuthenticatedLayout>
+</template>
