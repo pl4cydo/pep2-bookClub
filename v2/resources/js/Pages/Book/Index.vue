1727f6f2d52e3c6d9215e297c57c7baaaa88ad23
pl4cydo <psl2@discente.ifpe.edu.br>
2023-06-04 11:41:54 -0300

    new branch

--- v2/resources/js/Pages/Book/Index.vue
+++ v2/resources/js/Pages/Book/Index.vue
@@ -1,3 +1,34 @@
+<script setup>
+import AuthenticatedLayout from '@/Layouts/AuthenticatedLayout.vue';
+import { Head } from '@inertiajs/vue3';
+</script>
+
 <template>
-    
+    <Head title="Book" />
+
+    <AuthenticatedLayout>
+        <!-- <template #header>
+            <h2 class="font-semibold text-xl text-gray-800 leading-tight">Book</h2>
+        </template> -->
+
+        <div class="py-12">
+            <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
+                <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
+                    <!-- <div class="p-6 text-gray-900">You're logged in!</div> -->
+                    <div class="bloco">
+
+                    </div>
+                </div>
+            </div>
+        </div>
+    </AuthenticatedLayout>
 </template>
+
+<style>
+    .bloco {
+        border: 1px solid black;
+        width: auto;
+        height: 2000px;
+    }
+
+</style>
